1618aa7f74fd4275b4906d0922b93f9d42eaa598
SunkaraboinaPraveenKumar <praveenkumargitsunkaraboina@gmail.com>
2024-08-30 09:54:14 +0530

    shiva

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const allowedOrigins = [
 
 const corsOptions = {
     origin: function (origin, callback) {
-        if (allowedOrigins.includes(origin)) {
+        if (allowedOrigins.includes(origin) || !origin) {
             callback(null, true);
         } else {
             callback(new Error('Not allowed by CORS'));
